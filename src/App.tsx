06c6021fca77d0c95c208dced5df201c2b67f68e
KiranMantha <kiranv.mantha@gmail.com>
2024-05-13 13:11:04 +0530

    added env-cmd to pass environment variables during local development instead of dotenv

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Build mode: {process.env.NODE_ENV}</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
